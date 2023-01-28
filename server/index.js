a1c3a9a88cbf6e157422d6e99229732b7f402023
deepsalunkhee <99858248+deepsalunkhee@users.noreply.github.com>
2023-01-28 14:16:06 +0530

    version 1.8

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const app=express();
-const port=5000;
+const port="mern-bro-food-backend-35sed8x90-deepsalunkhee.vercel.app" || 5000;
 const mongoDB=require('./db');
 const cors = require('cors')
 
