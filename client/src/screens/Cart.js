0de97384de66bdb7025dca1604bfb23f4a55fa0f
deepsalunkhee <99858248+deepsalunkhee@users.noreply.github.com>
2023-01-28 15:18:52 +0530

    version 1.17

--- client/src/screens/Cart.js
+++ client/src/screens/Cart.js
@@ -21,7 +21,7 @@ export default function Cart() {
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
     // console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("http://localhost:5000/api/orderData", {
+    let response = await fetch("https://mern-bro-food-backend.vercel.app/api/orderData", {
      
       method: 'POST',
       headers: {
